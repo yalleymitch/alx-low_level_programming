7fc8bf13dd75d3fb7a976560ae5f97fd24729d5c
yalleymitch <yalley622@gmail.com>
2023-04-28 17:47:44 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
@@ -12,17 +11,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
  list_t *new;
- unsigned int x = 0;
+ unsigned int len = 0;
 
- while (str[x])
- x++;
+ while (str[len])
+ len++;
 
  new = malloc(sizeof(list_t));
  if (!new)
  return (NULL);
 
  new->str = strdup(str);
- new->x = x;
+ new->len = len;
  new->next = (*head);
  (*head) = new;
 
