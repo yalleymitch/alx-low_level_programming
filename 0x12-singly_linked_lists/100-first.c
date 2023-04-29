f7e6866e8dcc5b2a4609c96f280e560ffc8b9a10
yalleymitch <yalley622@gmail.com>
2023-04-28 19:36:00 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,11 +4,11 @@ void first(void) __attribute__ ((constructor));
 
 /**
  * first -It prints a phrase prior to the main function being called.
- * 
+ *
  */
 
 void first(void)
 {
-     printf("You're beat! and yet, you must allow,\n");
-     printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
