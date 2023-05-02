065e4092693d57a4c7d72de2122b0ab48b89b281
yalleymitch <yalley622@gmail.com>
2023-05-02 09:50:04 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,11 +3,11 @@
 /**
  * add_nodeint_end - it definitely adds a node at the end of a linked list
  * @head: it points to the first element in the list
- * @e: data to insert in the new element
+ * @n: data to insert in the new element
  *
  * Return: pointer to the new node when successful, or NULL if it fails
  */
-listint_t *add_nodeint_end(listint_t **head, const int e)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int e)
 	if (!new)
 		return (NULL);
 
-	new->e = e;
+	new->n = n;
 	new->next = NULL;
 
 	if (*head == NULL)
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,23 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * struct listint_s - it's a list that is only linked once
- * @e: it is a number or an integer
+ * @n: it is a number or an integer
  * @next: it displays or identifies the next node
  * Description: Structure of an individual linked list node for the Alx project
  *
  */
 	typedef struct listint_s
 	{
-	int e;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int e);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
@@ -30,7 +29,7 @@ int sum_listint(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int e);
-listint_t *add_nodeint_end(listint_t **head, const int e);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif
