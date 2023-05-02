980c3e64a9111cf1a7b3b67b495580a96307c496
yalleymitch <yalley622@gmail.com>
2023-05-02 11:05:56 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,14 +4,15 @@
  * free_listint - this Frees a linked list
  * @head: listint_t list is to be freed.
  */
+
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
 	while (head)
-	{
-		free(head);
-		temp = head->next;
-		head = temp;
+{
+	temp = head->next;
+	free(head);
+	head = temp;
 	}
 }
